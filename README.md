737411ac44a8de7f125f0455a79c06d39211e3b8
jany <my.maithi@gmail.com>
2019-08-12 22:11:09 +0800

    Update README

--- README.md
+++ README.md
@@ -1,47 +1,3 @@
-## Coding Assignment
-
-### Purpose
-We dont judge the correctness of your solutions.
-We want to see how you architect your app, and what your coding style is.
-
-### Contents
-
-We want to set permissions dynamically to each role for any `attributes`, `objects`, `classes`.
-Then through out our system we can easily check user's permissions by refering to their roles.
-
-We have 4 permissions as Creatable/Updatable/Readable/Destroyable
-
-We have 4 roles
-- sysadmin/director by default has full permissions.
-- Manager by default can only Readable
-- Staff by default has no permissions
-
-### requirements
-
-We want you to solve these cases:
-
-1. Limit access of Manager to base allowances data:
-
-  Each employee has many base allowances, 1 base allowance includes these attributes base_salary, overtime_payment, bonus, transport. Manager can only see base_salary.
-  
- 2. Limit access of Manager to Survey:
-
-  Users are allowed to create survey freely, each survey has some questions. For example, SurveyA will have question1 and question2. Now that user want the Manager can only see the question1.
-  
-3. Limit access of Manager to Setting page:
-  
-  We have a setting page where includes various confidential settings, our users want to allow the Manager to have access to only some pages. For example, we have `employee pages`, `Credit card pages`, then Managers can only see `employee pages`.
-  
-4. The users want a central setting page where they can easily change the permissions as described in 1,2,3.
-
-### Bonus points
-- Doing proper error handling, adding unit tests, and commenting your code.
-- Add TODOs and FIXMEs in places where you cut a corner for expediency but know a production ready version would need refactoring, additional tests or handling of certain edge cases that you know or suspect to exist but don't handle yet.
-
-### NOTE
-- We prepared for you models: company, employee, role.
-- Please CLONE this repo, implement and create your OWN REPO FOR SUBMISSION, send us the link to your repo. DO NOT CREATE PULL REQUEST TO THIS REPO.
-
 # RESULT
 
 ### Prepared
@@ -50,29 +6,85 @@ We want you to solve these cases:
 
 - install npm package
 
+### Tech stacks
+
+**Backend**
+- rails
+- rspec
+
+**Front end**
+- ReactJS
+- Ant Design Library
+
 ### How to run
 
-#### open 1 tab to run db
+#### Setup the database
 
 ```
 rails db:drop db:create db:migrate db:seed
 ```
 
-#### open 1 tab to run rails server
+#### Run rails server
 
 ```
   rails s
-
 ```
 
-#### open 1 tab to run web pack
+#### Run web pack server
 
 ```
   ./bin/webpack-dev-server
 ```
 
+### Run Test cases
+
+```
+  rspec
+```
+
+#### Images describe
+
+**Setting**
+
+> User can select IAM
+
+> From the company
+
+> Click on View Button to see the result
+
+<img width="1524" alt="Screenshot 2019-08-12 at 9 48 57 PM" src="https://user-images.githubusercontent.com/6791942/62869868-2c9a0900-bd4b-11e9-86b1-5052fc8ff587.png">
+
+**Setting Result**
+
+<img width="1483" alt="BtoaFeatureAssignments" src="https://user-images.githubusercontent.com/6791942/62870162-b77b0380-bd4b-11e9-9358-2d532c8bbd1e.png">
+
+**Manage setting**
+
+> Select table to setting
+
+<img width="1484" alt="Screenshot 2019-08-12 at 9 55 46 PM" src="https://user-images.githubusercontent.com/6791942/62870286-fc9f3580-bd4b-11e9-8f07-1499681dfec1.png">
+
+> Multiple setting
+
+<img width="1477" alt="BtoaFeatureAssignments" src="https://user-images.githubusercontent.com/6791942/62870403-2b1d1080-bd4c-11e9-96e6-c289d69c56d9.png">
+
+> Successful notification
+
+<img width="1532" alt="Screenshot 2019-08-12 at 9 59 05 PM" src="https://user-images.githubusercontent.com/6791942/62870552-6ddee880-bd4c-11e9-81d0-4db6af683f8e.png">
+
+> No permissions to manage setting
+
+<img width="1519" alt="Screenshot 2019-08-12 at 10 01 08 PM" src="https://user-images.githubusercontent.com/6791942/62870727-b5657480-bd4c-11e9-99b4-9136541be9ba.png">
+
 ### Pros and Cons
 
-#### Cons 
+#### Pros
 
-There are no setting for authorization
+- The controller is testable and the methods, functions are easy to read
+- Flexible UI design
+- Initialize seed db
+
+#### Cons
+
+- Within 2 days I haven't finished 4 cases BUT my approach solves for the whole situation
+- There are no setting for authorization
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,79 +10,80 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_811_091_223) do
+ActiveRecord::Schema.define(version: 20190811091223) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'access_scopes', force: :cascade do |t|
-    t.bigint 'role_id'
-    t.string 'table_name'
-    t.string 'field_access', array: true
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['role_id'], name: 'index_access_scopes_on_role_id'
+  create_table "access_scopes", force: :cascade do |t|
+    t.bigint "role_id"
+    t.string "table_name"
+    t.string "field_access", array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_access_scopes_on_role_id"
   end
 
-  create_table 'companies', force: :cascade do |t|
-    t.string 'name', default: '', null: false
-    t.datetime 'created_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
-    t.datetime 'updated_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
+  create_table "companies", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
   end
 
-  create_table 'employees', force: :cascade do |t|
-    t.string 'last_name', default: '', null: false
-    t.string 'first_name', default: '', null: false
-    t.string 'email', default: '', null: false
-    t.integer 'gender', limit: 2, default: 0, null: false
-    t.bigint 'company_id', null: false
-    t.datetime 'created_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
-    t.datetime 'updated_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
-    t.bigint 'role_id'
-    t.index ['company_id'], name: 'index_employees_on_company_id'
-    t.index ['role_id'], name: 'index_employees_on_role_id'
+  create_table "employees", force: :cascade do |t|
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.integer "gender", limit: 2, default: 0, null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.bigint "role_id"
+    t.index ["company_id"], name: "index_employees_on_company_id"
+    t.index ["role_id"], name: "index_employees_on_role_id"
   end
 
-  create_table 'incomes', force: :cascade do |t|
-    t.bigint 'employee_id'
-    t.string 'currency'
-    t.bigint 'base_salary'
-    t.bigint 'overtime_payment'
-    t.bigint 'bonus'
-    t.bigint 'transport'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['employee_id'], name: 'index_incomes_on_employee_id'
+  create_table "incomes", force: :cascade do |t|
+    t.bigint "employee_id"
+    t.string "currency"
+    t.bigint "base_salary"
+    t.bigint "overtime_payment"
+    t.bigint "bonus"
+    t.bigint "transport"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employee_id"], name: "index_incomes_on_employee_id"
   end
 
-  create_table 'permissions', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "permissions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'role_permissions', force: :cascade do |t|
-    t.bigint 'role_id'
-    t.bigint 'permission_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['permission_id'], name: 'index_role_permissions_on_permission_id'
-    t.index ['role_id'], name: 'index_role_permissions_on_role_id'
+  create_table "role_permissions", force: :cascade do |t|
+    t.bigint "role_id"
+    t.bigint "permission_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["permission_id"], name: "index_role_permissions_on_permission_id"
+    t.index ["role_id"], name: "index_role_permissions_on_role_id"
   end
 
-  create_table 'roles', force: :cascade do |t|
-    t.string 'name'
-    t.string 'resource_type'
-    t.bigint 'resource_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[name resource_type resource_id], name: 'index_roles_on_name_and_resource_type_and_resource_id'
-    t.index ['name'], name: 'index_roles_on_name'
-    t.index %w[resource_type resource_id], name: 'index_roles_on_resource_type_and_resource_id'
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
-  add_foreign_key 'access_scopes', 'roles'
-  add_foreign_key 'employees', 'roles'
-  add_foreign_key 'incomes', 'employees'
-  add_foreign_key 'role_permissions', 'permissions'
-  add_foreign_key 'role_permissions', 'roles'
+  add_foreign_key "access_scopes", "roles"
+  add_foreign_key "employees", "roles"
+  add_foreign_key "incomes", "employees"
+  add_foreign_key "role_permissions", "permissions"
+  add_foreign_key "role_permissions", "roles"
 end
